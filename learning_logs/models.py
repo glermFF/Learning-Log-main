ac498702a567d78b2bb591e050e9c9f24cc1af1e
Guilherme Ferreira <gffreitas13@gmail.com>
2024-03-19 20:23:32 -0300

    "new entrys"

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,3 +6,14 @@ class Topic(models.Model): # Topic the user is learning about
 
     def __str__(self) -> str:
         return self.text
+
+class Entry(models.Model): # Something specific about the topic
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    verbose_name_plural = "entries"
+
+def __str__(self):
+    return f"{self.text[50]}"
