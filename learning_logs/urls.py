3ae085038a651650baf42e5609a1d8f6dcd68caf
Guilherme Ferreira <gffreitas13@gmail.com>
2024-05-05 00:30:42 -0300

    "modifying forms | adding entries"

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     # Detailed page for each topic
     path('topics/<str:topic_id>/', views.topic, name='topic'),
     # Adding a new Topic
-    path('new_topic/', views.new_topic, name='new_topic')
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 # Create your views here.
 
 def index(request):
@@ -22,13 +22,29 @@ def topic(request, topic_id):
 
 def new_topic(request):
     """Add New Topic"""
-    if request.method != "POST": # No data, return a blank form;
+    if request.method != 'POST': # No data, return a blank form;
         form = TopicForm()
     else: # Data submitted, process data
         form = TopicForm(data=request.POST)
     if form.is_valid():
         form.save()
-        return redirect('learning_logs:topic')
+        return redirect('learning_logs:topics')
     
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add New Entry"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST': # No data, create new blank form;
+        form = EntryForm()
+    else: # Data submitted, process data;
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+        
+    context = {'topic':topic, 'form':form}
+    return render(request, 'learning_logs/new_entry.html', context)
