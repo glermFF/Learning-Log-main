c66e2556b47d15457bdda61ffde761f7db8ac361
Guilherme Ferreira <gffreitas13@gmail.com>
2024-05-30 11:46:07 -0300

    "Log in/Log out pages"

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'learning_logs', # My app
+    'users', # My app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+"""URL patterns for users"""
+
+from django.urls import path, include
+
+app_name = 'users'
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+]
